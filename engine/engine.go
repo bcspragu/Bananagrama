2f055b5a09fbc9d1632605a1d7cd4fdfc5774706
Brandon Sprague <brandon00sprague@gmail.com>
2016-10-02 20:53:50 -0700

    Some passing tests, and some broken code

--- engine/engine.go
+++ engine/engine.go
@@ -1,5 +1,7 @@
 package engine
 
+import "sort"
+
 type Orientation int
 
 const (
@@ -8,6 +10,18 @@ const (
 	Vertical
 )
 
+type byX []Word
+
+func (x byX) Len() int           { return len(x) }
+func (x byX) Less(i, j int) bool { return x[i].Loc.X < x[j].Loc.Y }
+func (x byX) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+type byY []Word
+
+func (y byY) Len() int           { return len(y) }
+func (y byY) Less(i, j int) bool { return y[i].Loc.Y < y[j].Loc.Y }
+func (y byY) Swap(i, j int)      { y[i], y[j] = y[j], y[i] }
+
 type Board struct {
 	Words []Word
 }
@@ -15,12 +29,16 @@ type Board struct {
 type Word struct {
 	Orientation Orientation
 	Text        string
-	X, Y        uint8
+	Loc         Loc
 }
 
 type CharLoc struct {
 	Letter string
 	Loc    Loc
+}
+
+type Loc struct {
+	X, Y int
 }
 
 func (w Word) CharLocs() []CharLoc {
@@ -32,31 +50,136 @@ func (w Word) CharLocs() []CharLoc {
 	case Vertical:
 		dy = 1
 	}
-	for i := range w.Text {
+	for i := 0; i < len(w.Text); i++ {
 		cls[i] = CharLoc{
 			Letter: w.Text[i : i+1],
-			Loc:    {X: w.X + i*dx, Y: w.Y + i*dy},
+			Loc:    Loc{X: w.Loc.X + i*dx, Y: w.Loc.Y + i*dy},
 		}
 	}
 	return cls
 }
 
-type Loc struct {
-	X, Y uint8
+// Which X coordinates do these words share?
+// again
+//     extra
+//   zit
+func (w Word) sharedX(o Word) []int {
+	xs := []int{}
+	// Iterate through the first word, see which indexes are contained by the second
+	// TODO(bsprague) There are likely more efficient interval calculation methods
+	for i := 0; i < len(w.Text); i++ {
+		if o.containsX(i + w.Loc.X) {
+			xs = append(xs, i+w.Loc.X)
+		}
+	}
+	return xs
 }
 
-func (b Board) Valid(letters []string) bool {
+func (w Word) containsX(i int) bool {
+	// If the given index is at or after the first letter, but is before the end,
+	// then it's contained
+	return i >= w.Loc.X && i < (w.Loc.X+len(w.Text))
+}
+
+func (w Word) letterAt(x int) string {
+	if x < w.Loc.X || x > (w.Loc.X+len(w.Text)-1) {
+		return ""
+	}
+	i := x - w.Loc.X
+	return w.Text[i : i+1]
+}
+
+func (b *Board) Valid(letters []string) bool {
 	letterMap := make(map[Loc]string)
 
 	for _, word := range b.Words {
 		for _, cl := range word.CharLocs() {
 			// If we've already placed a letter there, they've sent us a malformed
 			// board
-			if l, ok := letterMap; ok && l != cl.Letter {
+			if l, ok := letterMap[cl.Loc]; ok && l != cl.Letter {
 				return false
 			} else if !ok {
 				letterMap[cl.Loc] = cl.Letter
 			}
 		}
 	}
+	// TODO(bsprague): Check that they're all real words, and check for words created by juxtaposition
+	return true
+}
+
+func (b *Board) implicitWords() []string {
+	// The algorithm:
+	// 1. Filter down to only one orientation
+	// 2. Bucket items by opposite axis (eg filter to horizontal and bucket by Y coordinate
+	// 3. Look for sequential indices, build words from overlap
+	// 4. Reset once overlap breaks
+	h, _ := b.byOrientation()
+
+	sort.Sort(byX(h))
+	hm := make(map[int][]Word)
+
+	for _, word := range h {
+		if ws, ok := hm[word.Loc.Y]; ok {
+			hm[word.Loc.Y] = append(ws, word)
+		} else {
+			hm[word.Loc.Y] = []Word{word}
+		}
+	}
+	keys := make([]int, len(hm))
+	i := 0
+	for y := range hm {
+		keys[i] = y
+		i++
+	}
+	sort.Ints(keys)
+	start := false
+	//sm := make(map[int][]string)
+	for i, cur := range keys[:len(keys)-1] {
+		next := keys[i+1]
+		// We've found words in adjacent rows
+		if next-cur == 1 {
+			// This is the first adjacent row
+			if !start {
+				//for x, s := range findOverlapX(hm[cur], hm[next]) {
+				// TODO(bsprague): Fill in this section, starting new words when
+				// necessary, and splitting words when necessary
+				//}
+			}
+		}
+	}
+
+	return []string{}
+}
+
+func findOverlapX(l1, l2 []Word) map[int]string {
+	m := make(map[int]string)
+	for _, w1 := range l1 {
+		for _, w2 := range l2 {
+			if w1.Loc.X > (w2.Loc.X + len(w2.Text) - 1) {
+				// Skip this one, we haven't reached the word yet
+				continue
+			}
+			if w2.Loc.X > (w1.Loc.X + len(w1.Text) - 1) {
+				// Skip this and all after, since we're sorted by X, none of these overlap
+				break
+			}
+			for _, x := range w1.sharedX(w2) {
+				// w1 and w2 overlap here, lets get their letters
+				m[x] = w1.letterAt(x) + w2.letterAt(x)
+			}
+		}
+	}
+	return m
+}
+
+func (b *Board) byOrientation() (h []Word, v []Word) {
+	for _, word := range b.Words {
+		switch word.Orientation {
+		case Horizontal:
+			h = append(h, word)
+		case Vertical:
+			v = append(v, word)
+		}
+	}
+	return
 }
--- engine/engine_test.go
+++ engine/engine_test.go
@@ -1,10 +1,37 @@
 package engine
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestCharLocs(t *testing.T) {
 	testcases := []struct {
 		word Word
-		want []CharLocs
-	}{}
+		want []CharLoc
+	}{
+		{
+			word: Word{
+				Orientation: Horizontal,
+				Text:        "test",
+				Loc:         Loc{X: 1, Y: 2},
+			},
+			want: []CharLoc{{"t", Loc{1, 2}}, {"e", Loc{2, 2}}, {"s", Loc{3, 2}}, {"t", Loc{4, 2}}},
+		},
+		{
+			word: Word{
+				Orientation: Vertical,
+				Text:        "again",
+				Loc:         Loc{X: 2, Y: 4},
+			},
+			want: []CharLoc{{"a", Loc{2, 4}}, {"g", Loc{2, 5}}, {"a", Loc{2, 6}}, {"i", Loc{2, 7}}, {"n", Loc{2, 8}}},
+		},
+	}
+
+	for _, tc := range testcases {
+		got := tc.word.CharLocs()
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("word %s: got %v, want %v", tc.word.Text, got, tc.want)
+		}
+	}
 }
