c5ba0565bb04384fa45e00b1dd21a51edaba5e05
Brandon Sprague <brandon00sprague@gmail.com>
2016-10-04 18:35:26 -0700

    Dictionary tests

---
+++ engine/dictionary_test.go
@@ -0,0 +1,27 @@
+package engine
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestHasWord(t *testing.T) {
+	testcases := []struct {
+		input string
+		want  map[string]bool
+	}{
+		{"cookies\ncream", map[string]bool{"cookies": true, "cream": true, "cake": false}},
+		{"", map[string]bool{"cookies": false, "cream": false, "cake": false}},
+		{"dogs", map[string]bool{"dog": false, "dogs": true, "doggo": false, "dogsy": false}},
+	}
+
+	for _, tc := range testcases {
+		dict := NewDictionary(strings.NewReader(tc.input))
+		for word, want := range tc.want {
+			got := dict.HasWord(word)
+			if want != got {
+				t.Errorf("HasWord(%s): got %t, want %t", word, got, want)
+			}
+		}
+	}
+}
