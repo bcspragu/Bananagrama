84cbf96f5a4a29772e56171815d28bc565ff339f
Brandon Sprague <bsprague@google.com>
2020-01-04 19:33:06 -0800

    Persist our ECDSA key that we use for auth
    Addresses more of #8, now tokens will still be valid after the server
    restarts. The DB still isn't persisted, which is a related but slightly
    different issue.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,7 +4,11 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	cryptorand "crypto/rand"
+	"crypto/x509"
 	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"log"
 	"math/rand"
 	"net"
@@ -22,9 +26,10 @@ import (
 )
 
 var (
-	addr     = flag.String("addr", ":8080", "http service address")
-	apiAddr  = flag.String("api_addr", ":8079", "RPC server address")
-	dictPath = flag.String("dict", "", "path to the dictionary file to use")
+	addr        = flag.String("addr", ":8080", "http service address")
+	apiAddr     = flag.String("api_addr", ":8079", "RPC server address")
+	dictPath    = flag.String("dict", "", "path to the dictionary file to use")
+	authKeyPath = flag.String("auth_key_path", "auth.key", "path to the PEM-encoded ECDSA key to use for signing/verifying JWT tokens")
 )
 
 func main() {
@@ -41,10 +46,7 @@ func main() {
 		log.Fatalf("failed to load dictionary %q: %v", *dictPath, err)
 	}
 
-	pk, err := ecdsa.GenerateKey(elliptic.P256(), cryptorand.Reader)
-	if err != nil {
-		log.Fatalf("failed to generate private key: %v", err)
-	}
+	pk, err := loadOrGenerateKey(*authKeyPath)
 
 	authClient := auth.New(pk)
 
@@ -75,4 +77,50 @@ func loadDict(fn string) (banana.Dictionary, error) {
 	defer f.Close()
 
 	return banana.NewDictionary(f)
+}
+
+func loadOrGenerateKey(keyPath string) (*ecdsa.PrivateKey, error) {
+	f, err := os.Open(keyPath)
+	if os.IsNotExist(err) {
+		return generateAndSaveKey(keyPath)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("failed to read key file: %v", err)
+	}
+	defer f.Close()
+
+	// File exists, load from it.
+	return loadKey(f)
+}
+
+func generateAndSaveKey(keyPath string) (*ecdsa.PrivateKey, error) {
+	pk, err := ecdsa.GenerateKey(elliptic.P256(), cryptorand.Reader)
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate private key: %v", err)
+	}
+
+	dat, err := x509.MarshalECPrivateKey(pk)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshal key: %v", err)
+	}
+
+	if err := ioutil.WriteFile(keyPath, dat, 0600); err != nil {
+		return nil, fmt.Errorf("failed to write auth key: %v", err)
+	}
+
+	return pk, nil
+}
+
+func loadKey(r io.Reader) (*ecdsa.PrivateKey, error) {
+	dat, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read auth key: %v", err)
+	}
+
+	pk, err := x509.ParseECPrivateKey(dat)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse key: %v", err)
+	}
+
+	return pk, nil
 }
